7a4e16e5746618c64f8d39468829e77e4b6218ba
Randheerrrk <randheerrrk@gmail.com>
2021-06-15 18:09:22 +0530

    init

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://97bbfd06eea9.ngrok.io'
+  uri: 'https://ccback.azurewebsites.net/'
 });
 
 const authLink = setContext(() => {
